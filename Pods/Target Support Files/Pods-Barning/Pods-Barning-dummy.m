4e4c08f0d93ef617f0ce60f528029ef89195dcb8
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-06-14 16:56:42 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-Barning/Pods-Barning-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Barning : NSObject
+@end
+@implementation PodsDummy_Pods_Barning
+@end
---
+++ Pods/Target Support Files/Pods-BarningUITests/Pods-BarningUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BarningUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BarningUITests
+@end
